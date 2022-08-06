eaf5db9ee4dd0bb73bc50b61495cdd74051340ed
jeniluo <Jeniluo@163.com>
2022-08-06 20:25:45 +0800

    webFiles

--- PHP/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $file = $_FILES['track']['tmp_name'];
-    $filename = $_FILES['track']['name'];
-    $path = 'test/';
-    move_uploaded_file($file,$path.$filename);
-
-?>
--- WXXCX/notify.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo json(array('code'=> 1, 'data'=>'支付成功'));
-?>
